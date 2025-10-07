8f3f3fcf3b5a184683b65f2a427f2a0c4ac19cab
Sérgio Ricardo Vieira Torres Silva <working7.sergio@gmail.com>
2025-10-07 10:14:52 -0300

    Update

---
+++ 01-introducao-r-studio/Atividade/1_atv.R
@@ -0,0 +1,11 @@
+#1.Crie um script com os dados fictícios de uma pequena empresa (nome, receita, despesa)
+empresa <- "Torres LTDA"
+receita <- 350000
+despesa <- 200000
+
+
+#2.calcule o lucro líquido
+lucro_liquido = receita - despesa
+
+#3.Use o print() para mostrar os resultados com uma frase explicativa
+print(paste("A empresa", empresa, "teve um lucro de R$", lucro_liquido))
